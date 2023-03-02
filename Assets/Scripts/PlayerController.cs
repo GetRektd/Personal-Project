4860f1724b968e54acafd610c633d0b6a15a66f7
GetRektd <lgibb24@aoiths.org>
2023-03-02 10:03:33 -0500

    save

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 0.2f;
+
+    public float verticalInput;
+
+    public float rotationSpeed = 75;
+
+    Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * 0.2f);
+        transform.Rotate(Vector3.left * rotationSpeed * verticalInput * Time.deltaTime);
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        
+
+     
+
+       
+        
+    }
+}
